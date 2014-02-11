24e5f119ec801b412e42557f185afbc601f4b251
Dave Cohen <dcohen@moontoast.com>
2014-02-10 19:37:28 -0500

    - Adding device detection endpoint, interceptors and argument resolvers

--- README.md
+++ README.md
@@ -1,8 +1,10 @@
 spring4-playground-webmvc
 =========================
 
-Playground for Spring 4 WebMVC functionality
+Playground for Spring 4 WebMVC functionality, with some Spring Mobile device detection thrown in just for kicks.
 
 Once deployed, http://localhost:8080/spring-playground-webmvc/ will display the highly impressive "Hello" message.
 
 http://localhost:8080/spring-playground-webmvc/bye will display a goodbye message that tugs at the heart-strings.
+
+http://localhost:8080/spring-playground-webmvc/device
--- src/main/java/com/levelcap/spring/playground/webmvc/config/WebMvcConfig.java
+++ src/main/java/com/levelcap/spring/playground/webmvc/config/WebMvcConfig.java
@@ -1,11 +1,47 @@
 package com.levelcap.spring.playground.webmvc.config;
 
+import java.util.List;
+
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.mobile.device.DeviceResolverHandlerInterceptor;
+import org.springframework.mobile.device.DeviceWebArgumentResolver;
+import org.springframework.mobile.device.site.SitePreferenceWebArgumentResolver;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.mvc.method.annotation.ServletWebArgumentResolverAdapter;
 
 @EnableWebMvc
 @Configuration
 class WebMvcConfig extends WebMvcConfigurerAdapter
 {
+    //Add a deviceResolverHandlerInterceptor Bean to the Web Configuration for Device handling
+    @Bean
+    public DeviceResolverHandlerInterceptor deviceResolverHandlerInterceptor() {
+        return new DeviceResolverHandlerInterceptor();
+    }
+    
+    //Add argument resolvers to allow Device as a Controller parameter in DeviceDetectionController
+    @Override
+    public void addArgumentResolvers(
+            List<HandlerMethodArgumentResolver> argumentResolvers)
+    {
+        // Adding Spring mobile argument resolvers
+        argumentResolvers.add(
+                new ServletWebArgumentResolverAdapter(
+                        new DeviceWebArgumentResolver()));
+
+        argumentResolvers.add(
+                new ServletWebArgumentResolverAdapter(
+                        new SitePreferenceWebArgumentResolver()));
+    }
+    
+
+    //Add the interceptor created in the bean above
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(deviceResolverHandlerInterceptor());
+    }    
 }
